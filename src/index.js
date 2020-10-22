89c531efbecce08bea00395e4e603f823d7ffa25
VadymKas <v.kaschin@gmail.com>
2020-10-22 22:38:19 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function reverse (n) {
-  
+        let numToString = n.toString();
+        let reversedString = '';
+        let result = '';
+        
+        for (var i = 0; i < numToString.length; i += 1) {
+          if (numToString.slice(-1) === '0') {
+            reversedString = `${numToString[i]}${reversedString}`;
+            result = reversedString.slice(1);
+          } else if (numToString.slice(0, 1) === '-') {
+            reversedString = `${numToString[i]}${reversedString}`;
+            result = reversedString.slice(0, -1);
+          } else {
+            result = `${numToString[i]}${result}`;
+          }
+        }
+        return result;     
 }
